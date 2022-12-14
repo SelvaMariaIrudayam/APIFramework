569e6938b6400884fd52fc897a929a83a411846b
SelvaMariaIrudayam <selvaxavier.rajam@gmail.com>
2022-12-14 15:52:26 -0600

    master commit

---
+++ src/test/java/MainTest/MasterTest1.java
@@ -0,0 +1,11 @@
+package MainTest;
+
+import org.testng.annotations.Test;
+
+public class MasterTest1 {
+	@Test
+	public static void masterTest1() {
+		System.out.println("welcome to MasterTest1");
+	}
+
+}
---
+++ src/test/java/MainTest/MasterTest2.java
@@ -0,0 +1,11 @@
+package MainTest;
+
+import org.testng.annotations.Test;
+
+public class MasterTest2 {
+		@Test
+		public static void master_test2() {
+			System.out.println("welcome to master_test2");
+		}
+
+}
