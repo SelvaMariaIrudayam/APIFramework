e016bd429f1de11722a2837487b98318b7591b15
SelvaMariaIrudayam <selvaxavier.rajam@gmail.com>
2022-12-14 15:56:58 -0600

    branch commit

---
+++ src/test/java/BranchPack/BranchTest1.java
@@ -0,0 +1,11 @@
+package BranchPack;
+
+import org.testng.annotations.Test;
+
+public class BranchTest1 {
+	@Test
+	public static void branchTest1() {
+		System.out.println("welcome to branch Test1");
+	}
+
+}
